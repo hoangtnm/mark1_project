4df8c5fea2ca6d9d1b449e47c4ef8706927205ad
hoangtnm <minhhoangtrannhat@gmail.com>
2019-09-03 10:41:01 +0700

    Refactor code

--- cook_data.py
+++ cook_data.py
@@ -1,13 +1,14 @@
-#%matplotlib inline
+# %matplotlib inline
+import os
+import h5py
 import numpy as np
 import pandas as pd
-import h5py
+from PIL import Image
+from PIL import ImageDraw
 from matplotlib import use
 use("TkAgg")
-import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
-import os
 import Cooking
+import matplotlib.pyplot as plt
 
 # chunk size for training batches
 chunk_size = 32
@@ -22,17 +23,18 @@ RAW_DATA_DIR = './raw_data/'
 COOKED_DATA_DIR = './cooked_data/'
 
 # Choose The folders to search for data under RAW_DATA_DIR
-COOK_ALL_DATA = True  
+COOK_ALL_DATA = True
 
 data_folders = []
 
-#if COOK_ALL_DATA is set to False, append your desired data folders here
+# if COOK_ALL_DATA is set to False, append your desired data folders here
 # data_folder.append('folder_name1')
 # data_folder.append('folder_name2')
 # ...
 if COOK_ALL_DATA:
-	data_folders = [name for name in os.listdir(RAW_DATA_DIR)]
+    data_folders = [name for name in os.listdir(RAW_DATA_DIR)]
 
 
 full_path_raw_folders = [os.path.join(RAW_DATA_DIR, f) for f in data_folders]
-Cooking.cook(full_path_raw_folders, COOKED_DATA_DIR, train_eval_test_split, chunk_size)
+Cooking.cook(full_path_raw_folders, COOKED_DATA_DIR,
+             train_eval_test_split, chunk_size)
