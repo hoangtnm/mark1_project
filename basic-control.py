1d591f4a757432b17e8eff736ed984adac81215e
hoangtnm <minhhoangtrannhat@gmail.com>
2019-09-24 18:32:01 +0700

    update basic-control

--- basic-control.py
+++ basic-control.py
@@ -33,46 +33,48 @@ if __name__ == '__main__':
                    1                       1
     """
 
-    while True:
-        car_state = client.getCarState()
-        print(f'Speed: {car_state.speed} Gear: {car_state.gear}')
-        if (car_state.speed < MAX_SPEED):
-            car_controls.throttle = 1.0
-        else:
-            car_controls.throttle = 0.0
+    try:
+        while True:
+            car_state = client.getCarState()
+            print(f'Speed: {car_state.speed} Gear: {car_state.gear}')
+            if (car_state.speed < MAX_SPEED):
+                car_controls.throttle = 1.0
+            else:
+                car_controls.throttle = 0.0
 
-        _, axis_data, _ = controller.read()
-        raw_speed = axis_data[1]
-        raw_theta = axis_data[3]
-        speed = np.interp(-raw_speed, (-1, 1), (-MAX_SPEED, MAX_SPEED))
-        theta = np.interp(raw_theta, (-1, 1), (-MAX_THETA, MAX_THETA))
+            _, axis_data, _ = controller.read()
+            raw_speed = axis_data[1]
+            raw_theta = axis_data[3]
+            speed = np.interp(-raw_speed, (-1, 1), (-MAX_SPEED, MAX_SPEED))
+            theta = np.interp(raw_theta, (-1, 1), (-MAX_THETA, MAX_THETA))
 
-        # car_controls.throttle = speed
-        car_controls.steering = theta
-        client.setCarControls(car_controls)
+            # car_controls.throttle = speed
+            car_controls.steering = theta
+            client.setCarControls(car_controls)
 
-        # # Getting images
-        # responses = client.simGetImages([
-        #     # png format
-        #     airsim.ImageRequest("0", airsim.ImageType.Scene),
-        #     # uncompressed RGB array bytes
-        #     airsim.ImageRequest("1", airsim.ImageType.Scene, False, False),
-        #     # floating point uncompressed image
-        #     airsim.ImageRequest("1", airsim.ImageType.DepthPlanner, True)
-        # ])
+            # # Getting images
+            # responses = client.simGetImages([
+            #     # png format
+            #     airsim.ImageRequest("0", airsim.ImageType.Scene),
+            #     # uncompressed RGB array bytes
+            #     airsim.ImageRequest("1", airsim.ImageType.Scene, False, False),
+            #     # floating point uncompressed image
+            #     airsim.ImageRequest("1", airsim.ImageType.DepthPlanner, True)
+            # ])
 
-        # response = responses[1]
-        # # get numpy array
-        # img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
-        # # reshape array to 3 channel image array H X W X 3
-        # img_rgb = img1d.reshape(response.height, response.width, 3)
-        
-        # cv2.imshow('image', img_rgb)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     cv2.destroyAllWindows()
-        #     break
+            # response = responses[1]
+            # # get numpy array
+            # img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
+            # # reshape array to 3 channel image array H X W X 3
+            # img_rgb = img1d.reshape(response.height, response.width, 3)
+            
+            # cv2.imshow('image', img_rgb)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     cv2.destroyAllWindows()
+            #     break
 
-    # restore to original state
-    client.reset()
+    except KeyboardInterrupt:
+        # restore to original state
+        client.reset()
 
-    client.enableApiControl(False)
+        client.enableApiControl(False)
